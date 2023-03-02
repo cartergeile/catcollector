876ff8189ec348ae5318ca4756e78eabca0330f5
Carter Geile <cartergeile@l.local>
2023-03-02 10:50:36 -0600

    adds base html and styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Define the home view function
+def home(request):
+  return render(request, 'home.html')
+
+# about route
+def about(request):
+  return render(request, 'about.html')
